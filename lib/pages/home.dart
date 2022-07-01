4d801702efdbb54a7b565b7e830c27b311bee621
HeadTriXz <l.knol@outlook.com>
2022-07-01 20:13:45 +0200

    Fix for rest

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:app/helpers/globals.dart';
+import 'package:app/helpers/restApi.dart';
 import 'package:app/libs/positioning/positioning.dart';
 import 'package:app/pages/nfc.dart';
 import 'package:flutter/material.dart' hide Route;
@@ -15,22 +16,22 @@ import 'package:flutter_svg/flutter_svg.dart';
  * [60, 600]
  */
 
-Point audioOne = Point(60, 185, soundRange: 30, soundFile: '/static/audio/jr-1.mp3');
-Point audioTwo = Point(300, 460, soundRange: 30, soundFile: '/static/audio/jr-2.mp3');
+// Point audioOne = Point(60, 185, soundRange: 30, soundFile: '/static/audio/jr-1.mp3');
+// Point audioTwo = Point(300, 460, soundRange: 30, soundFile: '/static/audio/jr-2.mp3');
 
-Route testRoute = Route(
-  name: 'JR Chronicles',
-  description: 'Ontdek de iconische projecten van de internationale bekende franse kunstenaar JR',
-  thumbnail: 'https://www.groningermuseum.nl/media/2/Tentoonstellingen/2021/JR/_1200x670_crop_center-center_95_none/JR.-GIANTS-Kikito-and-the-Border-Patrol-Tecate-Mexico-U.S.A.-2017.jpg',
-  parts: [
-    Line(Point(60, 75), audioOne, 50),
-    Line(audioOne, Point(135, 315), 30),
-    Line(Point(135, 315), Point(300, 315), 30),
-    Line(Point(300, 315), audioTwo, 40),
-    Line(audioTwo, Point(190, 600), 40),
-    Line(Point(190, 600), Point(60, 600), 30)
-  ]
-);
+// Route testRoute = Route(
+//   name: 'JR Chronicles',
+//   description: 'Ontdek de iconische projecten van de internationale bekende franse kunstenaar JR',
+//   thumbnail: 'https://www.groningermuseum.nl/media/2/Tentoonstellingen/2021/JR/_1200x670_crop_center-center_95_none/JR.-GIANTS-Kikito-and-the-Border-Patrol-Tecate-Mexico-U.S.A.-2017.jpg',
+//   parts: [
+//     Line(Point(60, 75), audioOne, 50),
+//     Line(audioOne, Point(135, 315), 30),
+//     Line(Point(135, 315), Point(300, 315), 30),
+//     Line(Point(300, 315), audioTwo, 40),
+//     Line(audioTwo, Point(190, 600), 40),
+//     Line(Point(190, 600), Point(60, 600), 30)
+//   ]
+// );
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -56,8 +57,8 @@ class HomeScreen extends StatelessWidget {
                   ]
                 )
               ),
-              Routes([testRoute])
-              // Routes(snapshot.data ?? [])
+              // Routes([testRoute])
+              Routes(snapshot.data ?? [])
             ]
           )
         );
@@ -87,7 +88,7 @@ class Routes extends StatelessWidget {
             minVerticalPadding: 25,
             leading: ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(5)),
-              child: Image.network(routes[index].thumbnail)
+              child: Image.network(RestClient.baseUrl + routes[index].thumbnail)
             ),
             title: Text(
               routes[index].name, style: themeData.textTheme.headline6),
