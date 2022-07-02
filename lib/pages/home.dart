700fdfbd082459de978b7688c5029c16170fc90e
Michel Gerding <michelgerding@gmail.com>
2022-07-02 13:37:57 +0200

    added reload for routes
    Signed-off-by: Michel Gerding <michelgerding@gmail.com>

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,9 +20,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 // Point audioTwo = Point(300, 460, soundRange: 30, soundFile: '/static/audio/jr-2.mp3');
 
 // Route testRoute = Route(
-//   name: 'JR Chronicles',
-//   description: 'Ontdek de iconische projecten van de internationale bekende franse kunstenaar JR',
-//   thumbnail: 'https://www.groningermuseum.nl/media/2/Tentoonstellingen/2021/JR/_1200x670_crop_center-center_95_none/JR.-GIANTS-Kikito-and-the-Border-Patrol-Tecate-Mexico-U.S.A.-2017.jpg',
+// name: 'JR Chronicles',
+// description: 'Ontdek de iconische projecten van de internationale bekende franse kunstenaar JR',
+// thumbnail: 'https://www.groningermuseum.nl/media/2/Tentoonstellingen/2021/JR/_1200x670_crop_center-center_95_none/JR.-GIANTS-Kikito-and-the-Border-Patrol-Tecate-Mexico-U.S.A.-2017.jpg',
 //   parts: [
 //     Line(Point(60, 75), audioOne, 50),
 //     Line(audioOne, Point(135, 315), 30),
@@ -33,122 +33,108 @@ import 'package:flutter_svg/flutter_svg.dart';
 //   ]
 // );
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     ThemeData themeData = Theme.of(context);
 
     return FutureBuilder<List<Route>>(
-      builder: (context, snapshot) {
-        return Scaffold(
-          body: Column(
-            children: [
-              Header(size: size),
-              Padding(
+        builder: (context, snapshot) {
+          return Scaffold(
+              body: Column(children: [
+            Header(size: size),
+            Padding(
                 padding: EdgeInsets.only(top: 30.0, bottom: 10.0),
-                child: Column(
-                  children: [
-                    Text(
-                      'Huidige Routes',
-                      style: themeData.textTheme.headline1,
-                      textAlign: TextAlign.center,
-                    )
-                  ]
-                )
-              ),
-              // Routes([testRoute])
-              Routes(snapshot.data ?? [])
-            ]
-          )
-        );
-      },
-      future: restAPI.getAll()
-    );
+                child: Column(children: [
+                  Text(
+                    'Huidige Routes',
+                    style: themeData.textTheme.headline1,
+                    textAlign: TextAlign.center,
+                  )
+                ])),
+            // Routes([testRoute])
+            Routes(snapshot.data ?? [], () async {
+              setState(() {});
+            })
+          ]));
+        },
+        future: restAPI.getAll());
   }
 }
 
 class Routes extends StatelessWidget {
   final List<Route> routes;
+  final Future<void> Function() refresh;
 
-  const Routes(this.routes);
+  const Routes(this.routes, this.refresh);
 
   @override
   Widget build(BuildContext context) {
     ThemeData themeData = Theme.of(context);
     return Flexible(
+        child: RefreshIndicator(
+      onRefresh: refresh,
       child: ListView.builder(
-        itemCount: routes.length,
-        itemBuilder: (context, index) {
-          String description = routes[index].description 
-            ?? 'No description available...';
+          itemCount: routes.length,
+          itemBuilder: (context, index) {
+            String description =
+                routes[index].description ?? 'No description available...';
 
-          return ListTile(
-            visualDensity: VisualDensity(vertical: 4.0),
-            minVerticalPadding: 25,
-            leading: ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(5)),
-              child: Image.network(RestClient.baseUrl + routes[index].thumbnail)
-            ),
-            title: Text(
-              routes[index].name, style: themeData.textTheme.headline6),
-            subtitle: Text(
-              description,
-              style: themeData.textTheme.subtitle2,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2
-            ),
-            isThreeLine: true,
-            onTap: () {
-              Navigator.push(context, 
-                MaterialPageRoute(builder: (context) => NFCScreen(
-                  route: routes[index]
-                ))
-              );
-            }
-          );
-        },
-        padding: EdgeInsets.zero
-      )
-    );
+            return ListTile(
+                visualDensity: VisualDensity(vertical: 4.0),
+                minVerticalPadding: 25,
+                leading: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    child: Image.network(
+                        RestClient.baseUrl + routes[index].thumbnail)),
+                title: Text(routes[index].name,
+                    style: themeData.textTheme.headline6),
+                subtitle: Text(description,
+                    style: themeData.textTheme.subtitle2,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2),
+                isThreeLine: true,
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              NFCScreen(route: routes[index])));
+                });
+          },
+          padding: EdgeInsets.zero),
+    ));
   }
 }
 
 class Header extends StatelessWidget {
   final Size size;
 
-  const Header({
-    Key? key,
-    required this.size
-  }) : super(key: key);
+  const Header({Key? key, required this.size}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: ShapeDecoration(
-        color: Color(0xFFF4CC2D),
-        shape: CustomShape(size: size)
-      ),
-      height: size.height * 0.23,
-      child: Center(
-        child: Container(
-          child: SvgPicture.asset(
-            'assets/images/groningerMuseumLogo.svg',
-            color: Color(0xFF0F595B),
-            width: size.width * 0.8
-          )
-        )
-      )
-    );
+        decoration: ShapeDecoration(
+            color: Color(0xFFF4CC2D), shape: CustomShape(size: size)),
+        height: size.height * 0.23,
+        child: Center(
+            child: Container(
+                child: SvgPicture.asset('assets/images/groningerMuseumLogo.svg',
+                    color: Color(0xFF0F595B), width: size.width * 0.8))));
   }
 }
 
 class CustomShape extends ShapeBorder {
   final Size size;
 
-  const CustomShape({
-    required this.size
-  });
+  const CustomShape({required this.size});
 
   @override
   EdgeInsetsGeometry get dimensions => EdgeInsets.zero;
@@ -161,12 +147,7 @@ class CustomShape extends ShapeBorder {
     double height = size.height * 0.23;
     return Path()
       ..lineTo(0, height - 50)
-      ..quadraticBezierTo(
-        size.width / 4,
-        height + 50,
-        size.width,
-        height - 50
-      )
+      ..quadraticBezierTo(size.width / 4, height + 50, size.width, height - 50)
       ..lineTo(size.width, 0)
       ..close();
   }
