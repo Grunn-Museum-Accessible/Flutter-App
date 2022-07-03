59063348572efd6eff51103f362f0f0334f90c1d
Michel Gerding <michelgerding@gmail.com>
2022-07-03 20:21:33 +0200

    changes. changed api url, changed maxHeight homescreen
    Signed-off-by: Michel Gerding <michelgerding@gmail.com>

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,22 +16,40 @@ import 'package:flutter_svg/flutter_svg.dart';
  * [60, 600]
  */
 
-Point audioOne = Point(60, 185, soundRange: 30, soundFile: '/static/audio/jr-1.mp3');
-Point audioTwo = Point(300, 460, soundRange: 30, soundFile: '/static/audio/jr-2.mp3');
-
-Route testRoute = Route(
-name: 'JR Chronicles',
-description: 'Ontdek de iconische projecten van de internationale bekende franse kunstenaar JR',
-thumbnail: 'https://www.groningermuseum.nl/media/2/Tentoonstellingen/2021/JR/_1200x670_crop_center-center_95_none/JR.-GIANTS-Kikito-and-the-Border-Patrol-Tecate-Mexico-U.S.A.-2017.jpg',
-  parts: [
-    Line(Point(60, 75), audioOne, 50),
-    Line(audioOne, Point(135, 315), 30),
-    Line(Point(135, 315), Point(300, 315), 30),
-    Line(Point(300, 315), audioTwo, 40),
-    Line(audioTwo, Point(190, 600), 40),
-    Line(Point(190, 600), Point(60, 600), 30)
-  ]
-);
+// Route testRoute = Route(
+//     name: 'JR Chronicles',
+//     description:
+//         'Ontdek de iconische projecten van de internationale bekende franse kunstenaar JR',
+//     thumbnail:
+//         'https://www.groningermuseum.nl/media/2/Tentoonstellingen/2021/JR/_1200x670_crop_center-center_95_none/JR.-GIANTS-Kikito-and-the-Border-Patrol-Tecate-Mexico-U.S.A.-2017.jpg',
+//     parts: [
+//       Line(
+//           Point(60, 75),
+//           Point(60, 185,
+//               soundRange: 30,
+//               soundFile: '/storage/emulated/0/Download/jr-1.mp3'),
+//           50),
+//       Line(
+//           Point(60, 185,
+//               soundRange: 30,
+//               soundFile: '/storage/emulated/0/Download/jr-1.mp3'),
+//           Point(135, 315),
+//           30),
+//       Line(Point(135, 315), Point(300, 315), 30),
+//       Line(
+//           Point(300, 315),
+//           Point(300, 460,
+//               soundRange: 30,
+//               soundFile: '/storage/emulated/0/Download/jr-2.mp3'),
+//           40),
+//       Line(
+//           Point(300, 460,
+//               soundRange: 30,
+//               soundFile: '/storage/emulated/0/Download/jr-2.mp3'),
+//           Point(190, 600),
+//           40),
+//       Line(Point(190, 600), Point(60, 600), 30)
+//     ]);
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -58,12 +76,12 @@ class _HomeScreenState extends State<HomeScreen> {
                     textAlign: TextAlign.center,
                   )
                 ])),
-            Routes([testRoute], () async {
-              setState(() {});
-            })
-            // Routes(snapshot.data ?? [], () async {
+            // Routes([testRoute], () async {
             //   setState(() {});
             // })
+            Routes(snapshot.data ?? [], () async {
+              setState(() {});
+            })
           ]));
         },
         future: restAPI.getAll());
@@ -85,30 +103,36 @@ class Routes extends StatelessWidget {
       child: ListView.builder(
           itemCount: routes.length,
           itemBuilder: (context, index) {
-            String description =
-                routes[index].description ?? 'No description available...';
+            String description = (routes[index].description ?? '').trim();
 
-            return ListTile(
-                visualDensity: VisualDensity(vertical: 4.0),
-                minVerticalPadding: 25,
-                leading: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                    child: Image.network(
-                        RestClient.baseUrl + routes[index].thumbnail)),
-                title: Text(routes[index].name,
-                    style: themeData.textTheme.headline6),
-                subtitle: Text(description,
-                    style: themeData.textTheme.subtitle2,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2),
-                isThreeLine: true,
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              NFCScreen(route: routes[index])));
-                });
+            return SizedBox(
+              height: 99,
+              child: ListTile(
+                  visualDensity: VisualDensity(vertical: 4.0),
+                  minVerticalPadding: 25,
+                  leading: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      child: Image.network(
+                          RestClient.baseUrl + routes[index].thumbnail)),
+                  title: Text(routes[index].name,
+                      style: themeData.textTheme.headline6),
+                  subtitle: routes[index].description != ''
+                      ? Text(
+                          description,
+                          style: themeData.textTheme.subtitle2,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        )
+                      : null,
+                  isThreeLine: false,
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                NFCScreen(route: routes[index])));
+                  }),
+            );
           },
           padding: EdgeInsets.zero),
     ));
